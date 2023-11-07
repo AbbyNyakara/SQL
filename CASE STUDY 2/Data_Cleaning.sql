a635a444cab5ab9a54cb7fc190fe726a57592183
Abigael Nyakara <mogusuab@msu.edu>
2023-11-06 20:48:52 -0500

    add files

--- CASE STUDY 2/Data_Cleaning.sql
+++ CASE STUDY 2/Data_Cleaning.sql
@@ -4,7 +4,7 @@ use pizza_runner;
 show tables;
 
 # Customer_orders table
-drop table customer_orders;
+SET SQL_SAFE_UPDATES = 0;
 
 # Exclusions column
 UPDATE customer_orders
@@ -16,11 +16,6 @@ UPDATE customer_orders
 SET extras = NULL 
 WHERE extras = 'null' OR extras = '';
 
-# ADD THE PRIMARY KEY THAT THE FOREIGN KEYS IN OTHER TABLES WILL REFERENCE 
-
-ALTER TABLE customer_orders
-ADD CONSTRAINT pk1_customer_orders
-PRIMARY KEY (order_id);
 
 # pizza_names table
 
@@ -30,9 +25,33 @@ FOREIGN KEY (pizza_id)
 REFERENCES customer_orders(pizza_id);
 
 
+# Runner orders table 
+
+UPDATE runner_orders
+SET pickup_time = NULL
+WHERE pickup_time = 'null' OR  pickup_time = '';
+
+UPDATE runner_orders
+SET distance = NULL
+WHERE distance = 'null';
+
+UPDATE runner_orders
+SET duration = NULL
+WHERE duration = 'null';
+
+UPDATE runner_orders
+SET cancellation = NULL
+WHERE cancellation = 'null' OR cancellation = '';
+
+SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS 
+  WHERE table_name = 'runner_orders';
+
+select * from runner_orders;
 
 
-select * from pizza_recipes;
+
+
+
 
 
 
--- CASE STUDY 2/pizza_metrics.sql
+++ CASE STUDY 2/pizza_metrics.sql
@@ -2,56 +2,49 @@ use pizza_runner;
 show tables;
 # 1. How many pizzas were ordered?
 
-SELECT COUNT(order_id) AS pizzas_ordered
+SELECT COUNT(*) AS pizzas_ordered
 FROM customer_orders;
 # 14 pizzas were ordered. 
 
-# 2. How many unique customer orders were made?
-SELECT COUNT(DISTINCT order_id) as unique_orders
+#2. How many unique customer orders were made?
+SELECT COUNT(DISTINCT order_id) AS unique_customer_orders
 FROM customer_orders;
 
 # 10 unique customer orders
 
-# 3.How many successful orders were delivered by each runner?
+# 3. How many successful orders were delivered by each runner?
+SELECT runner_id, COUNT(order_id) AS number_of_orders
+FROM runner_orders
+WHERE distance != 0
+GROUP BY runner_id;
+
+# 4. How many of each type of pizza was delivered?
+
+SELECT  p.pizza_name, COUNT(c.pizza_id) as pizzas_ordered
+FROM customer_orders c
+JOIN pizza_names p
+ON c.pizza_id = p.pizza_id
+GROUP BY c.pizza_id;
+
+# 5. How many Vegetarian and Meatlovers were ordered by each customer?
+
+SELECT c.customer_id, p.pizza_name,COUNT(p.pizza_name) as pizzas_ordered
+FROM customer_orders c
+JOIN pizza_names p
+ON c.pizza_id = p.pizza_id
+GROUP BY c.customer_id, p.pizza_name
+ORDER BY c.customer_id;
+
+
+
+
+
+
+
+
+
+select * from customer_orders;
 select * from runner_orders;
-SET SQL_SAFE_UPDATES = 0;
-
-# Update the null values
-UPDATE runner_orders
-SET pickup_time = NULL
-WHERE pickup_time = 'null';
-
-# Clean the runner_orders table
-UPDATE runner_orders
-SET cancellation = NULL
-WHERE cancellation = 'null' OR cancellation = '';
-
-
-SHOW INDEX FROM customer_orders WHERE Column_name = 'order_id';
-
-
-
-
-ALTER TABLE parent_table
-ADD INDEX index_name (parent_column);
-
-
-DESCRIBE runner_orders;
-DESCRIBE customer_orders;
-# Add the foreign keys to the tables 
-ALTER TABLE runner_orders
-ADD FOREIGN KEY (order_id)
-REFERENCES customer_orders(order_id);
-
-
-# query: 
-SELECT r.runner_id, COUNT (c.order_id) AS orders_per_runner
-FROM runner_orders r
-JOIN customer_orders c 
-ON c.order_id = r.order_id
-WHERE r.cancellations IS NOT NULL
-GROUP BY runner_id
-;
 
 
 
